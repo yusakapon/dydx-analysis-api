d0cc76a7e16eb7125d4e2a68dcc08edc811766ae
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2022-03-06 13:43:51 +0900

    add: ide補完ツール追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (config('app.env') == 'local') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
