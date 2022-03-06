e0ede13a78db5e22faddb77fb68ee27282298075
tatsukoni_macPro2 <tatsukoni_macpro2@tatsukoni-macPro2noMacBook-Pro.local>
2022-03-06 13:33:51 +0900

    add: テスト & CI追加

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -3,8 +3,32 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected static $databaseSetup = false;
+
+    /**
+     * base setup
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        if (!static::$databaseSetup) {
+            $this->setUpDatabase();
+        }
+        static::$databaseSetup = true;
+    }
+
+    /**
+     * setup database
+     * @return void
+     */
+    protected function setUpDatabase(): void
+    {
+        Artisan::call('migrate:refresh', ['--force' => true]);
+    }
 }
